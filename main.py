cfd5226b626bd831a8119a34563faffc14a13588
Pliszka <karoliniakkjk@gmail.com>
2022-02-12 23:41:00 +0100

    sending SMS

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
 from private_data import *
+from twilio.rest import Client
 
 MY_LAT = 50.0915234
 MY_LON = 18.2199165
 parameters = {
-    'lat': -14.235004,
-    'lon': -51.925282,
+    'lat': MY_LAT,
+    'lon': MY_LON,
     'exclude': 'current,minutely,daily,alerts',
     'appid': my_key
 }
@@ -18,4 +19,11 @@ rain = False
 next_hours = data['hourly'][:12]
 
 if [True for hour in next_hours if hour['weather'][0]['id'] < 700]:
-    print('Bring an umbrella')
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+            body="Hey, it's going to rain. Don\'t forget your umbrella ☂️",
+            from_='+18126339019',
+            to=my_phone
+        )
+    print(message.status)
