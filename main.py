bcc3daf582dada8924d89a153dad4896ee2cb7fb
Pliszka <karoliniakkjk@gmail.com>
2022-02-14 13:45:46 +0100

    set env vars

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
-from private_data import *
+import os
+#from private_data import *
 from twilio.rest import Client
 
 MY_LAT = 50.0915234
@@ -8,7 +9,7 @@ parameters = {
     'lat': MY_LAT,
     'lon': MY_LON,
     'exclude': 'current,minutely,daily,alerts',
-    'appid': my_key
+    'appid': os.environ.get('my_key')
 }
 
 response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=parameters)
@@ -18,11 +19,11 @@ data = response.json()
 next_hours = data['hourly'][:12]
 
 if [True for hour in next_hours if hour['weather'][0]['id'] < 700]:
-    client = Client(account_sid, auth_token)
+    client = Client(os.environ.get('account_sid'), os.environ.get('auth_token'))
     message = client.messages \
         .create(
         body="Hey, it's going to rain. Don\'t forget your umbrella ☂️",
         from_='+18126339019',
-        to=my_phone
+        to=os.environ.get('my_phone')
         )
     print(message.status)
